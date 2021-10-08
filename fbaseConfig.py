c218872d44c6246bfb78cb4efe3c73a61e806925
ssjh23 <64569228+ssjh23@users.noreply.github.com>
2021-10-08 23:24:37 +0800

    Update fbaseConfig.py

--- fbaseConfig.py
+++ fbaseConfig.py
@@ -1,19 +1,5 @@
 firebaseConfig = {
 
-  "apiKey": "AIzaSyBzNZlMtHKE_pRILf_y9Edi2cQc7a6ocsM",
-
-  "authDomain": "fir-course-4a47b.firebaseapp.com",
-
- "databaseURL": "https://fir-course-4a47b-default-rtdb.firebaseio.com",
-
-  "projectId": "fir-course-4a47b",
-
-  "storageBucket": "fir-course-4a47b.appspot.com",
-
-  "messagingSenderId": "636092634333",
-
-  "appId": "1:636092634333:web:c66c9a0b6e659ace757734",
-
-  "measurementId": "G-W9EY705K9C"
+###FILL IN OWN FIREBASE CONFIG
 
 };
